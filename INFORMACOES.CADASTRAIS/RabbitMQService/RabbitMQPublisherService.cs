015326f989a3514d43bc45ff4cb57679e52ae0c7
mayardes <mayardesoliveira@gmail.com>
2022-11-29 16:04:08 -0400

    fix: Ajustando configurações do RabbitMQ

--- INFORMACOES.CADASTRAIS/RabbitMQService/RabbitMQPublisherService.cs
+++ INFORMACOES.CADASTRAIS/RabbitMQService/RabbitMQPublisherService.cs
@@ -11,26 +11,18 @@ namespace INFORMACOESCADASTRAIS.RabbitMQService
     {
         private readonly CadastroContext _dbContext;
 
-        public RabbitMQPublisherService(CadastroContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
         public Task<Cliente> SendModel(Cliente cliente)
         {
-            var RabbitMQServer = "localhost";
-
             var _factory = new ConnectionFactory()
             {
-                HostName = "localhost"
+                HostName = StaticConfigurationManager.AppSetting["RabbitMqSettings:Host"]
             };
 
-            //open an connection
             using (var connection = _factory.CreateConnection())
 
-            //create a channel
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: StaticConfigurationManager.AppSetting["RabbitMqSettings:QueueName"], //queue name
+                channel.QueueDeclare(queue: StaticConfigurationManager.AppSetting["RabbitMqSettings:QueueName"],
                                      durable: true,      //remains active when restarted server
                                      exclusive: false,    //only access on actual connection
                                      autoDelete: false,   //automatic deleted when consumers get message
