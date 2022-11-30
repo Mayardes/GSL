1a98bc4377b7828127dc279e008df5648365212b
mayardes <mayardesoliveira@gmail.com>
2022-11-29 22:57:37 -0400

    fix: Correção da comunicação entre os módulos

--- SISTEMA.LEGADO/Program.cs
+++ SISTEMA.LEGADO/Program.cs
@@ -12,25 +12,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Instancia do Banco de Dados
 builder.Services.AddDbContext<LegadoContext>();
+
+builder.Services.AddControllers();
+
 builder.Services.AddScoped<RabbitMQPublisherService<Cliente>>();
 builder.Services.AddHostedService<RabbitMQBackgroundConsumerService<Cliente>>();
 builder.Services.AddScoped<INotificationServer<Cliente>, NotificationServer<Cliente>>();
 
-
-builder.Services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
-builder.Services.AddSwaggerGen(options => {
-    options.ExampleFilters();
-});
-//services
 builder.Services.AddScoped<ClienteServices>();
 builder.Services.AddScoped<DepositoServices>();
 builder.Services.AddScoped<FornecedorServices>();
 builder.Services.AddScoped<MercadoriaServices>();
+
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//Swagger
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+builder.Services.AddSwaggerGen(options => {
+    options.ExampleFilters();
+});
+
 
 var app = builder.Build();
 
@@ -38,6 +46,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
